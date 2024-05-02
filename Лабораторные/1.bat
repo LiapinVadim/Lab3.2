4636ab50753fe16c100c0a088da17799f4dfa842
LiapinVadim <lapinvadym789@gmail.com>
2024-05-02 15:19:59 +0000

    Add files via upload

---
+++ Лабораторные/1.bat
@@ -0,0 +1,10 @@
+mkdir "Лабораторные\Группа\ФИО"
+cd "Лабораторные\Группа\ФИО"
+mkdir "batch"
+cd "batch"
+mkdir "Скрытая папка"
+echo. > "Не скрытая папка\copyhelp.txt"
+attrib +h "Скрытая папка"
+xcopy /? > "Не скрытая папка\copyhelp.txt"
+xcopy "Не скрытая папка\copyhelp.txt" "Скрытая папка\copied_copyhelp.txt"
+
